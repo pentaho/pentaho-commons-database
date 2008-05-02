25c54df68dc04ea1a0d1e058e8d51ea1c10f1c7a
Nick Baker <nbaker@pentaho.com>
2008-05-02 12:36:36 +0000

    Some tweaks for Swing quirks

--- src/org/pentaho/test/ui/database/SwingTest.java
+++ src/org/pentaho/test/ui/database/SwingTest.java
@@ -4,55 +4,68 @@ import java.io.InputStream;
 
 import org.dom4j.Document;
 import org.dom4j.io.SAXReader;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
 import org.pentaho.di.core.database.DatabaseMeta;
 import org.pentaho.ui.xul.XulDomContainer;
+import org.pentaho.ui.xul.XulException;
 import org.pentaho.ui.xul.containers.XulWindow;
 import org.pentaho.ui.xul.swing.SwingXulLoader;
+import org.pentaho.ui.xul.swt.SwtXulLoader;
 
 public class SwingTest {
 
-       DatabaseMeta database = null;
+	DatabaseMeta database = null;
 
-       public static void main(String[] args) {
+	public static void main(String[] args) {
 
-         SwingTest harness = new SwingTest();
-         
-         try {
-           InputStream in = DatabaseDialogHarness.class.getClassLoader()
-                 .getResourceAsStream("org/pentaho/ui/database/databasedialog.xul"); //$NON-NLS-1$
-           if (in == null) {
-             System.out.println("Invalid Input"); //$NON-NLS-1$
-             return;
-           }
-           
-           SAXReader rdr = new SAXReader();
-           final Document doc = rdr.read(in);
-           
-           harness.showDialog(doc);
-           
-         } catch (Exception e) {
-           e.printStackTrace();
-         }
+		SwingTest harness = new SwingTest();
 
-       }
-       
-       private void showDialog(final Document doc){
+		try {
+			InputStream in = DatabaseDialogHarness.class.getClassLoader()
+					.getResourceAsStream("org/pentaho/ui/database/databasedialog.xul");
+			if (in == null) {
+				System.out.println("Invalid Input");
+				return;
+			}
 
-         XulDomContainer container = null;
-         try {
-           container = new SwingXulLoader().loadXul(doc);
-           if (database != null){
-             container.getEventHandler("dataHandler").setData(database); //$NON-NLS-1$
-           }
-         } catch (Exception e) {
-           e.printStackTrace();
-         } 
-         XulWindow dialog = (XulWindow) container.getDocumentRoot().getRootElement();
-         container.initialize();
-         dialog.open();
+			SAXReader rdr = new SAXReader();
+			final Document doc = rdr.read(in);
 
-       
-       }
+			harness.showDialog(doc);
 
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-     }
+	}
+
+	private void showDialog(final Document doc) {
+
+		XulDomContainer container = null;
+		try {
+			container = new SwingXulLoader().loadXul(doc);
+			if (database != null) {
+				container.getEventHandler("dataHandler").setData(database);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		XulWindow dialog = (XulWindow) container.getDocumentRoot().getRootElement();
+		container.initialize();
+		dialog.open();
+		try{
+			Object data = container.getEventHandler("dataHandler").getData();
+			int i=0;
+			i++;
+		} catch(XulException e){
+			System.out.println("Error getting data");
+		}
+	}
+
+}
