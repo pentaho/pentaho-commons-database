6f6fa2c16c16b5ab4e39fdd017e67fbb33cc4231
Nick Baker <nbaker@pentaho.com>
2010-11-06 21:08:16 +0000

    [BISERVER-5309] Generic Database option does not work. Changing code to re-use existing Properties object in the case that the DatabaseInterface has already set options.

--- pentaho-database-gwt/src/org/pentaho/database/util/DatabaseUtil.java
+++ pentaho-database-gwt/src/org/pentaho/database/util/DatabaseUtil.java
@@ -8,13 +8,14 @@ import org.pentaho.di.core.database.DatabaseMeta;
 import org.pentaho.di.core.database.PartitionDatabaseMeta;
 
 public class DatabaseUtil {
-  
+
   public static DatabaseMeta convertToDatabaseMeta(IDatabaseConnection conn) {
     DatabaseMeta meta = new DatabaseMeta();
 
     meta.setDatabaseType(conn.getDatabaseType().getShortName());
-    Properties props = new Properties();
-    
+
+    Properties props = meta.getDatabaseInterface().getAttributes();
+
     for (String key : conn.getExtraOptions().keySet()) {
       props.put(BaseDatabaseMeta.ATTRIBUTE_PREFIX_EXTRA_OPTION + key, conn.getExtraOptions().get(key));
     }
@@ -22,11 +23,11 @@ public class DatabaseUtil {
     for (String key : conn.getConnectionPoolingProperties().keySet()) {
       props.put(BaseDatabaseMeta.ATTRIBUTE_POOLING_PARAMETER_PREFIX + key, conn.getConnectionPoolingProperties().get(key));
     }
-    
+
     for (String key : conn.getAttributes().keySet()) {
       props.put(key, conn.getAttributes().get(key));
     }
-    
+
     meta.setAttributes(props);
 
     meta.setName(conn.getName());
@@ -37,19 +38,19 @@ public class DatabaseUtil {
     meta.setUsername(conn.getUsername());
     meta.setPassword(conn.getPassword());
 
-    
+
     meta.setServername(conn.getInformixServername());
     meta.setDataTablespace(conn.getDataTablespace());
     meta.setIndexTablespace(conn.getIndexTablespace());
-    
+
     // addl
     if (conn.getConnectSql() != null) {
       meta.setConnectSQL(conn.getConnectSql());
     }
-    
+
     meta.setInitialPoolSize(conn.getInitialPoolSize());
     meta.setMaximumPoolSize(conn.getMaximumPoolSize());
-    
+
     if (conn.getSQLServerInstance() != null) {
       meta.setSQLServerInstance(conn.getSQLServerInstance());
     }
@@ -60,10 +61,10 @@ public class DatabaseUtil {
     meta.setStreamingResults(conn.isStreamingResults());
     meta.setUsingConnectionPool(conn.isUsingConnectionPool());
     meta.setUsingDoubleDecimalAsSchemaTableSeparator(conn.isUsingDoubleDecimalAsSchemaTableSeparator());
-    
+
     if (conn.getPartitioningInformation() != null) {
       PartitionDatabaseMeta pdmetas[] = new PartitionDatabaseMeta[conn.getPartitioningInformation().size()];
-      
+
       //TODO
       int c = 0;
       for (org.pentaho.database.model.PartitionDatabaseMeta pdmeta : conn.getPartitioningInformation()) {
@@ -71,15 +72,15 @@ public class DatabaseUtil {
       }
       meta.setPartitioningInformation(pdmetas);
     }
-      
-    
+
+
     if (conn.getChanged()) {
       meta.setChanged();
     }
-    
+
     return meta;
   }
-  
+
   public static PartitionDatabaseMeta convertToPartitionDatabaseMeta(org.pentaho.database.model.PartitionDatabaseMeta pdm) {
     PartitionDatabaseMeta pdmeta = new PartitionDatabaseMeta();
     pdmeta.setDatabaseName(pdm.getDatabaseName());
