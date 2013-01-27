24afa5896b1911c7a496a92c27aec59fb9d7eac9
Willaim E. Seyler <wseyler@pentaho.com>
2013-01-27 15:37:26 -0500

    BISERVER-7589: Updated wrapper classes for AutoBean

---
+++ pentaho-database-gwt/src/org/pentaho/ui/database/event/DefaultDatabaseConnectionPoolParameterList.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2007 Pentaho Corporation.  All rights reserved. 
+ * This software was developed by Pentaho Corporation and is provided under the terms 
+ * of the Mozilla Public License, Version 1.1, or any later version. You may not use 
+ * this file except in compliance with the license. If you need a copy of the license, 
+ * please go to http://www.mozilla.org/MPL/MPL-1.1.txt. The Original Code is the Pentaho 
+ * BI Platform.  The Initial Developer is Pentaho Corporation.
+ *
+ * Software distributed under the Mozilla Public License is distributed on an "AS IS" 
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or  implied. Please refer to 
+ * the license for the specific language governing your rights and limitations.
+ *
+ * @created Jan 15, 2013 
+ * @author wseyler
+ */
+
+
+package org.pentaho.ui.database.event;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.pentaho.database.model.DatabaseConnectionPoolParameter;
+import org.pentaho.database.model.IDatabaseConnectionPoolParameter;
+
+/**
+ * @author wseyler
+ *
+ */
+@XmlRootElement
+public class DefaultDatabaseConnectionPoolParameterList implements IDatabaseConnectionPoolParameterList {
+  private List<IDatabaseConnectionPoolParameter> databaseConnectionPoolParameters;
+  
+/* (non-Javadoc)
+   * @see org.pentaho.ui.database.event.IDatabaseConnectionPoolParameterList#getDatabaseConnectionPoolParameters()
+   */
+  @Override
+  public List<IDatabaseConnectionPoolParameter> getDatabaseConnectionPoolParameters() {
+    return databaseConnectionPoolParameters;
+  }
+
+  /* (non-Javadoc)
+   * @see org.pentaho.ui.database.event.IDatabaseConnectionPoolParameterList#getDatabaseConnectionPoolParameters(java.util.List)
+   */
+  @Override
+  @XmlElement(type=DatabaseConnectionPoolParameter.class)
+  public void setDatabaseConnectionPoolParameters(List<IDatabaseConnectionPoolParameter> databaseConnectionPoolParameters) {
+    this.databaseConnectionPoolParameters = databaseConnectionPoolParameters;
+  }
+
+}
